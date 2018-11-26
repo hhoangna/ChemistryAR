a60fe42b9039e3a861c03377ed094af2f89b58d5
HuyHoang <hhoangna@gmail.com>
2018-11-26 15:31:50 +0700

    Add File

---
+++ ChemistryAR/ChemistryAR/Classes/ViewControllers/Reaction/Cell/ReactionCell.swift
@@ -0,0 +1,26 @@
+//
+//  ReactionCell.swift
+//  ChemistryAR
+//
+//  Created by Admin on 11/26/18.
+//  Copyright © 2018 HHumorous. All rights reserved.
+//
+
+import UIKit
+
+class ReactionCell: BaseTbvCell {
+    
+    @IBOutlet weak var tvContent: UITextView?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
