4cf8508db04a2bb652df32834d00239d1d2b7e80
Toan - Nguyen Van <toan@horical.com>
2018-11-26 10:27:45 +0700

    update reaction

---
+++ ChemistryAR/ChemistryAR/Classes/Manager/Services/APIService/APIs/APIs + Reaction.swift
@@ -0,0 +1,29 @@
+//
+//  APIs + Reaction.swift
+//  ChemistryAR
+//
+//  Created by Admin on 11/21/18.
+//  Copyright © 2018 HHumorous. All rights reserved.
+//
+
+import Foundation
+
+extension BaseAPI {
+    
+    @discardableResult
+    func getReactionEquation(product:String, input:String, callback: @escaping APICallback<ReactionModel>) -> APIRequest{
+        var param: String = input.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
+        if product != "" {
+            let prStr = product.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+            param = "\(param)/\(prStr ?? "")"
+        }
+
+        
+        let str = "http://www.studyvn.com/chemistry/find-equation/\(param)?getJSON&type=public"
+        return request(method: .GET,
+                       serverURL: str,
+                       path: "",
+                       input: .empty,
+                       callback: callback)
+    }
+}
