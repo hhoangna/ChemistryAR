dec587f29b7655e9e44b0af92e3b01ec0f43ccca
Mach Van Nguyen <nguyen@horical.com>
2018-12-12 00:18:41 +0700

    update noti

--- ChemistryAR/ChemistryAR/Classes/AppDelegate/AppDelegate.swift
+++ ChemistryAR/ChemistryAR/Classes/AppDelegate/AppDelegate.swift
@@ -25,7 +25,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        SERVICES().firebase.setupFirebase()
         // Override point for customization after application launch.
         IQKeyboardManager.shared().isEnabled = true
         Configuration.enableConfiguration()
@@ -33,6 +32,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
         SERVICES().push.startPushNotifications()
         SERVICES().push.delegate = self
         
+        SERVICES().firebase.setupFirebase()
+
         checkStatusLogin()
 
         return true
